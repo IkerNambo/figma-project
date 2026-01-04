deb0ff272bec96ce8f633c28665b2b13df4d6586
ikernambo <ikernamboproject@gmail.com>
2026-01-04 05:14:28 -0600

    progressed the navbar and the home components, and added buttons to see the figma files

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,17 +4,31 @@ import { Link } from "react-router-dom";
 export default function Navbar(){
     return(
         <nav>
-            <ul className="flex gap-4 backdrop-blur-2xl bg-amber-300 w-fit p-3 m-auto">
-                <li>
-                    <Link rel="stylesheet" to="/pricing"> Pricing</Link>
-                </li>
+            <ul className="flex justify-center items-center m-auto gap-10 w-full bg-[#0e1018] text-white backdrop-blur-[10px] border-b-[0.5px] border-gray-800">
+                brand name
+                <ul className="flex gap-4 items-center backdrop-blur-2xl w-fit p-3">
                 <li>
                     <Link rel="stylesheet" to="/">Home</Link>
                 </li>
                 <li>
+                    <Link rel="stylesheet" to="/">Features</Link>
+                </li>
+                <li>
+                    <Link rel="stylesheet" to="/pricing">Pricing</Link>
+                </li>
+                <li>
                     <Link rel="stylesheet" to="/contact" onClick={() => {console.log('clicked')}}>contact</Link>
                 </li>
+                <li>
+                    <Link rel="stylesheet" to="/contact" onClick={() => {console.log('clicked')}}>Icon</Link>
+                </li>
+                <ul>
+                    <li><a href="figma_pages/pricing.png">Home Figma</a></li>
+                    <li><a href="figma_pages/home.png">Pricing Figma</a></li>
+                </ul>
             </ul>
+            </ul>
+            
         </nav>
     );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,22 @@ export default function Home(){
     return(
         <>
             <Navbar/>
-            WE ARE SO GETTING ITT!!!!
+            <div className="relative text-white h-screen bg-(--main-blue) w-full">
+
+                <div className="absolute bg-[#a9baff] w-200 h-100 left-1/2 -translate-x-1/2 rounded-full blur-[160px] opacity-17"/>
+
+                
+                <div>
+                    <div className=" flex flex-col m-auto justify-center items-center pt-40 gap-7">
+                        <h1 className="text-6xl font-bold">Build faster, Ship sooner.</h1>
+                        <p className="text-gray-200 text-center text-[26px]">Accelerate your development workflow with our powerful <br /> tools and frameworks.</p>
+                        <button className="w-70 h-17 rounded-[7px] bg-[#1871ff] text-[28px] font-semibold">Get Started</button>
+                    </div>   
+                 
+                        
+                </div>
+            </div>
+            
         </>
         )
 }
